91385c65ae004a16ff353148c8612cd315e722ec
Zi Nguyen <zinguyenn@gmail.com>
2018-03-29 15:19:57 -0400

    add smooth scroll

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-(function() {
+$(document).ready(init);
+
+function init() {
   var items = document.querySelectorAll(".timeline li");
+  var topBtn = document.getElementById("to-top");
 
   //check if element is in viewport
   function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
-    return rect.top >= 0 && 
-    rect.left >= 0 && 
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && 
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+    return rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth);
   }
 
   function displayItem() {
@@ -16,9 +19,27 @@
         items[i].classList.add("in-view");
       }
     }
+
+    onScroll();
+  }
+
+  function onScroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("to-top").style.display = "block";
+    } else {
+      document.getElementById("to-top").style.display = "none";
+    }
+  }
+
+  function toTop() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 500);
+    return false;
   }
 
   //listen for events
   window.addEventListener("load", displayItem);
   window.addEventListener("scroll", displayItem);
-})();
+  topBtn.addEventListener("click", toTop);
+}
